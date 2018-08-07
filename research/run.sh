337fa652c3f82ac08106d2fbc022d03c596628dd
henry <fandichao1998@163.com>
2018-08-07 17:33:08 +0800

    abcdf

--- research/run.sh
+++ research/run.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
 output_dir=/output  # 训练目录
-dataset_dir=/data/fandichao1998/objectdetection1 # 数据集目录，这里是写死的，记得修改
+dataset_dir=/data/fandichao1998/hw8 # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
@@ -25,9 +25,9 @@ pipeline_config_path=$output_dir/$config
 # rm -rvf $output_dir/*
 
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
-cp $dataset_dir/$config $pipeline_config_path
+cp $config $pipeline_config_path
 
-for i in {0..5}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
+for i in {0..3}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
     echo "############" $i "runnning #################"
     last=$[$i*100]
